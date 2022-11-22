71653170527f5aed528def7144dc6222b6d78a5e
brunamello <brunacrespomello@gmail.com>
2022-11-22 02:13:36 +0000

    2 - Functions and Scopes

--- dart_curso_dois.dart
+++ dart_curso_dois.dart
@@ -4,15 +4,56 @@ void main(){
   String color = "Green and Yellow";
   String flavor = "Sweet and Sour";
   int dayFromCollection = 40;
-  bool isLoosed;
+  bool isLoosed = funcIsLoosed(dayFromCollection);
 
-  if (dayFromCollection >= 30){
-    isLoosed = true;
-  } else {
-    isLoosed = false;
-  }
+  printLoose("Grape", 45, color: "purple");
 
+  int quantosDias = funcQuantosDiasMadura(dayFromCollection);
+  print(quantosDias);
 
-
+  funcRecursive(1, 10);
 
 }
+
+// Posicionais Obrigatorios (name, int)
+
+// Nomeados Opcionais ({String? color})
+
+// Parametros Padrao ({String? color = "purple"})
+
+// Modificador "required" ({required String? color})
+
+printLoose(String name, int days, {String? color}){
+  if(days >= 30){
+    print("The $name is loosed.");
+  } else {
+    print("The $name is not loosed.");
+  }
+
+  if(color != null){
+    print("$color is the color of the $name.");
+  }
+}
+
+bool funcIsLoosed(int days){
+  if (days >= 30){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+funcQuantosDiasMadura(int days){
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = days - diasParaMadura;
+  return quantosDiasFaltam;
+}
+
+funcRecursive(int cont, int stop){
+  print("$cont without StackOverflow");
+  if(cont >= stop){
+    return;
+  }
+  funcRecursive(cont + 1, stop);
+}
+
