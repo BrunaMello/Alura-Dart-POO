841a64561e62f2fc9dfb6ea2bda59045cab9758b
brunamello <brunacrespomello@gmail.com>
2022-11-22 01:43:36 +0000

    1 - POO

---
+++ dart_curso_dois.dart
@@ -0,0 +1,18 @@
+void main(){
+  String name = "Orange";
+  double weight = 100.2;
+  String color = "Green and Yellow";
+  String flavor = "Sweet and Sour";
+  int dayFromCollection = 40;
+  bool isLoosed;
+
+  if (dayFromCollection >= 30){
+    isLoosed = true;
+  } else {
+    isLoosed = false;
+  }
+
+
+
+
+}
